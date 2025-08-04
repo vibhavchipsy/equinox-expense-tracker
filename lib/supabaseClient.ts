43d158c4c77aee1e157b7f6e4cb4186b7538dfef
vibhavchipsy <vibhavchipsy@gmail.com>
2025-08-04 15:07:19 +0530

    supabase

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+// lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://dxvbrfzqbvlsnwtiaxrc.supabase.co';
+const supabaseAnonKey = 'yeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dmJyZnpxYnZsc253dGlheHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyOTg4ODMsImV4cCI6MjA2OTg3NDg4M30.kOpbDP4yQ99rxJ_0Vq1KR67jVOuPUjNfsrGjbLNbVYQ';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
